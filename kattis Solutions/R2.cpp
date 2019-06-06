0083dc267a5cf77e92e09b8b4ce1e777b7a23e62
anas-itu <51449264+anas-itu@users.noreply.github.com>
2019-06-06 17:54:25 +0300

    Create R2.cpp

---
+++ kattis Solutions/R2.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int S;
+    int R1;
+    int R2;
+    cin >> R1 >> S;
+    R2 = (S * 2) - R1;
+    cout << R2 << endl;
+    return 0;
+}
