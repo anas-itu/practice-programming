c1545d58b240e883ca66a51e7f7a61843a9eeaa8
anas-itu <51449264+anas-itu@users.noreply.github.com>
2019-06-06 17:04:39 +0300

    Create Quadrant Selection.cpp

---
+++ kattis Solutions/Quadrant Selection.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cin >> x >> y;
+    if(x > 0 && y > 0)
+    {
+        cout << 1 << endl;
+    }
+    if(x < 0 && y > 0)
+    {
+        cout << 2 << endl;
+    }
+    if(x < 0 && y < 0)
+    {
+        cout << 3 << endl;
+    }
+    if(x > 0 && y < 0)
+    {
+        cout << 4 << endl;
+    }
+    return 0;
+}
