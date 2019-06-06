6d3483caf47d36cf5b9b54126cc8c53ed2fef6f4
anas-itu <51449264+anas-itu@users.noreply.github.com>
2019-06-06 18:28:11 +0300

    Create Take Two Stones.cpp

---
+++ kattis Solutions/Take Two Stones.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >>
+    N;
+    if (N % 2 == 0)
+    {
+        puts("Bob");
+    }
+    else
+    {
+        puts("Alice");
+    }
+    return 0;
+}
