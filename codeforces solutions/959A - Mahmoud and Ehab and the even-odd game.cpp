194d85813584c16c645e50b7efeada15b4627753
anas-itu <51449264+anas-itu@users.noreply.github.com>
2019-06-26 16:35:15 +0300

    Create 959A - Mahmoud and Ehab and the even-odd game.cpp

---
+++ codeforces solutions/959A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int num;
+	cin>>num;
+	if(num%2 == 0)
+		return (!printf("Mahmoud\n"));
+	else
+		return (!printf("Ehab\n"));
+
+	return 0;
+}
