fc5cb6cd89ef2c0478bc70c899f33b324a797b87
anas-itu <51449264+anas-itu@users.noreply.github.com>
2019-06-27 03:34:39 +0300

    Create Herman.cpp

---
+++ kattis Solutions/Herman.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <bits/stdc++.h>
+#include <sstream>
+using namespace std;
+const double pi = acos(-1);
+
+int main() {
+	int x;
+	cin>>x;
+	cout<<fixed<<setprecision(6)<<pi*x*x<<"\n";
+	cout<<fixed<<setprecision(6)<<x*x*2*1.0;
+	return 0;
+}
