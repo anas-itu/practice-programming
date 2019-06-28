e16e573318721582819c951a26ba4bf876191ea9
anas-itu <51449264+anas-itu@users.noreply.github.com>
2019-06-28 16:06:53 +0300

    Create Kornislav.cpp

---
+++ kattis Solutions/Kornislav.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> v;
+    for(int i = 0; i < 4; ++i){
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(), v.end());
+    cout << (v[0] * v[2]) << endl;
+
+    return 0;
+}
