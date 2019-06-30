fc96f0e6d530247c0776a26d0d5e4f9d8603286b
anas-itu <51449264+anas-itu@users.noreply.github.com>
2019-07-01 00:46:01 +0300

    Create Quick Estimates.cpp

---
+++ kattis Solutions/Quick Estimates.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <vector>
+const double pi = acos(-1);
+using namespace std;
+
+int main() {
+	int num;
+	string x;
+	cin>>num;
+	for(int i=0; i<num; ++i) {
+		cin>>x;
+		cout<<(int)x.size()<<endl;
+	}
+
+	return 0;
+}
