13c78b0087a4fd347ea3ad7da016bdda44900631
anas-itu <51449264+anas-itu@users.noreply.github.com>
2019-07-08 19:25:19 +0300

    Create A New Alphabet.cpp

---
+++ kattis Solutions/A New Alphabet.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <vector>
+const double pi = acos(-1);
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin,s);
+	for(int i=0; i<(int)s.length(); ++i){
+		if(s[i] == 'a' || s[i] == 'A')
+			cout << '@';
+		else if(s[i] == 'b' || s[i] == 'B')
+			cout << '8';
+		else if(s[i] == 'c' || s[i] == 'C')
+			cout << '(';
+		else if(s[i] == 'd' || s[i] == 'D')
+			cout << "|)";
+		else if(s[i] == 'e' || s[i] == 'E')
+			cout << '3';
+		else if(s[i] == 'f' || s[i] == 'F')
+			cout << '#';
+		else if(s[i] == 'g' || s[i] == 'G')
+			cout << '6';
+		else if(s[i] == 'h' || s[i] == 'H')
+			cout << "[-]";
+		else if(s[i] == 'i' || s[i] == 'I')
+			cout << '|';
+		else if(s[i] == 'j' || s[i] == 'J')
+			cout << "_|";
+		else if(s[i] == 'k' || s[i] == 'K')
+			cout << "|<";
+		else if(s[i] == 'l' || s[i] == 'L')
+			cout << "1";
+		else if(s[i] == 'm' || s[i] == 'M')
+			cout << "[]\\/[]";
+		else if(s[i] == 'n' || s[i] == 'N')
+			cout << "[]\\[]";
+		else if(s[i] == 'o' || s[i] == 'O')
+			cout << '0';
+		else if(s[i] == 'p' || s[i] == 'P')
+			cout << "|D";
+		else if(s[i] == 'q' || s[i] == 'Q')
+			cout << "(,)";
+		else if(s[i] == 'r' || s[i] == 'R')
+			cout << "|Z";
+		else if(s[i] == 's' || s[i] == 'S')
+			cout << '$';
+		else if(s[i] == 't' || s[i] == 'T')
+			cout << "']['";
+		else if(s[i] == 'u' || s[i] == 'U')
+			cout << "|_|";
+		else if(s[i] == 'v' || s[i] == 'V')
+			cout << "\\/";
+		else if(s[i] == 'w' || s[i] == 'W')
+			cout << "\\/\\/";
+		else if(s[i] == 'x' || s[i] == 'X')
+			cout << "}{";
+		else if(s[i] == 'y' || s[i] == 'Y')
+			cout << "`/";
+		else if(s[i] == 'z' || s[i] == 'Z')
+			cout<<'2';
+		else
+			cout << s[i];
+	}
+	return 0;
+}
